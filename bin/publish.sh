1f334c5ecd5cbc78c8c5c81dbe4317a1bfdacccb
Eric Johnson <eric.git@iijo.org>
2012-08-25 14:38:38 +0100

    More automation.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ruhoh compile && git co gh-pages && cp -r compiled/* . && rm -r compiled && git add -A && git st
+ruhoh compile && git co gh-pages && cp -r compiled/* . && rm -r compiled && git add -A && git st && git commit -a -m 'More' && git co master && git br
 
