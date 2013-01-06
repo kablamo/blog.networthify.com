b8985da72519c0cbb699acaf914db94d3f084ef0
Eric Johnson <eric.git@iijo.org>
2013-01-06 20:02:51 +0000

    More readable.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-ruby -I /home/eric/code/ruhoh.rb/lib /home/eric/code/ruhoh.rb/bin/ruhoh compile && git co gh-pages && cp -r compiled/* . && rm -r compiled && git add -A && git status && git commit -a -m 'More' && git co master && git push && git branch
+ruby -I /home/eric/code/ruhoh.rb/lib /home/eric/code/ruhoh.rb/bin/ruhoh compile && \
+git co gh-pages && \
+cp -r compiled/* . && \
+rm -r compiled && \
+git add -A && \
+git status && \
+git commit -a -m 'More' && \
+git push && \
+git co master && \
+git push && \
+git branch
 
