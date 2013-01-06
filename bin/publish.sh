209f6c4b83005bb4e5e2d01b333c8bdb4fb13a24
Eric Johnson <eric.git@iijo.org>
2013-01-06 19:52:53 +0000

    Changed 'git st' to 'git status'.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ruby -I /home/eric/code/ruhoh.rb/lib /home/eric/code/ruhoh.rb/bin/ruhoh compile && git co gh-pages && cp -r compiled/* . && rm -r compiled && git add -A && git st && git commit -a -m 'More' && git co master && git push && git br
+ruby -I /home/eric/code/ruhoh.rb/lib /home/eric/code/ruhoh.rb/bin/ruhoh compile && git co gh-pages && cp -r compiled/* . && rm -r compiled && git add -A && git status && git commit -a -m 'More' && git co master && git push && git br
 
